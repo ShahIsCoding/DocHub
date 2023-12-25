41743b0c7d0693eced1ba025785a04028f12c74f
ShahIsCoding <cpabhishekshah0036@hotmail.com>
2023-12-25 19:57:58 +0530

    started on whiteboard

--- client/src/components/pages/Whiteboard.js
+++ client/src/components/pages/Whiteboard.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Whiteboard = () => {
-  return <div>Whiteboard</div>;
+  const canvasRef = useRef(null);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  function randomColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    return "#" + randomColor;
+  }
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.beginPath();
+    context.moveTo(window.innerWidth / 2, window.innerHeight / 2);
+    context.lineTo(mousePosition.x, mousePosition.y, 2, 2);
+    context.strokeStyle = randomColor();
+    context.stroke();
+  }, [mousePosition]);
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePosition({
+        x: e.pageX,
+        y: e.pageY,
+      });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  });
+  return (
+    <canvas
+      width={window.innerWidth}
+      height={window.innerHeight}
+      style={{ background: "black" }}
+      ref={canvasRef}
+    ></canvas>
+  );
 };
 
 export default Whiteboard;
